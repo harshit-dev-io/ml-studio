b8f8990ef58c1b2b8712baecbfc4f3c1fa453829
Harshit <harshit392615@gmail.com>
2026-03-24 05:39:46 +0000

    chore : add firebase configuration

---
+++ src/backend/ml_studio/core/firebase.py
@@ -0,0 +1,18 @@
+import firebase_admin
+from firebase_admin import credentials
+from django.conf import settings
+import json
+import os
+
+def firebase_initialization():
+
+    if not firebase_admin._apps:
+        try:
+            firebase_cred_env = os.getenv("FIREBASE_CREDS")
+            firebase_creds = json.loads(firebase_cred_env)
+            cred = credentials.Certificate(firebase_creds)
+            firebase_admin.initialize_app(cred)
+            print("done")
+        except:
+            print("Invlaid firebase credentials")
+    
