2ffe58469e680f7f5646749480fe95f8577978ee
Harshit <harshit392615@gmail.com>
2026-03-25 06:45:50 +0000

    feat: integrate firebase auth and database logic into signup api

--- src/backend/ml_studio/accounts/views.py
+++ src/backend/ml_studio/accounts/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status 
+from .services import Get_or_Create_User
 # Create your views here.
 
 
@@ -11,7 +12,18 @@ class Signup_API(APIView):
             id_token = auth_token.split(" ")[1]
         except:
             return Response({"error" : "invalid credentials"} , status = 401)
-        # TODO: Add signup logic and Firebase verification here
+        try:
+            user , created = Get_or_Create_User(id_token=id_token)
+
+            if not user :
+                return Response({"error": "Invalid token or database error"}, status=status.HTTP_401_UNAUTHORIZED)
+            
+            if created:
+                return Response({"message": "Account created successfully"}, status=status.HTTP_201_CREATED)
+            else:
+                return Response({"message": "Account already exists"}, status=status.HTTP_200_OK)
+        except:
+            pass
         return Response({"message":"this is a response"} , status = status.HTTP_202_ACCEPTED)
 
         
