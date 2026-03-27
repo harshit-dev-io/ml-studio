d68466b258ec3501bee3726949a78df0966bd4b2
Harshit <harshit392615@gmail.com>
2026-03-27 17:07:52 +0000

    feat(core): create secure Tenant_Base_API for protected view inheritance

--- src/backend/ml_studio/organization/views.py
+++ src/backend/ml_studio/organization/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status 
 # Create your views here.
+
+class Tenant_Base_API(APIView):
+    def initial(self , request , *args , **kwargs):
+        if request.user and request.user.is_authenticated:
+            return super().initial(request,*args,**kwargs)
+        else:
+            return Response({"error": "Authentication required"}, status = status.HTTP_401_UNAUTHORIZED)
+
