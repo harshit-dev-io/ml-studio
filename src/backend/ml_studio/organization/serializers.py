b3a7fd418d8b619148a18d8ce566fc5e8c8e0f2e
Harshit <harshit392615@gmail.com>
2026-03-27 17:20:18 +0000

    feat(orgs): add organization list serializers for listing organization

---
+++ src/backend/ml_studio/organization/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Organization , Membership
+
+class Organization_List_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organization
+        fields = ['name' , 'slug' , 'type']
+
