5480cf55259e2239e21dea5f428b26ffef23a2e3
Harshit <harshit392615@gmail.com>
2026-03-27 17:22:19 +0000

    chore(admin): register organization and membership models

--- src/backend/ml_studio/organization/admin.py
+++ src/backend/ml_studio/organization/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Organization , Membership
 # Register your models here.
+
+@admin.register(Organization)
+class Organization_Admin(admin.ModelAdmin):
+    list_display = ['name' ,'slug', 'type' , 'created_at' , 'is_active' , 'is_archived' , 'is_deleted']
+    search_fields = ['name' , 'slug']
+    readonly_fields = ['created_at']
+
+@admin.register(Membership)
+class Membership_Admin(admin.ModelAdmin):
+    list_display = ['organization' , 'user' , 'role' , 'joined_at']
+    search_fields = ['organization__name' , 'user__email' , 'role']
+    readonly_fields = ['joined_at']
