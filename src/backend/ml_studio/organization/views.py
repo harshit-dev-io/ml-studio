c43ddd29564eb199b76d3d0dec2c35a3c206c732
Harshit <harshit392615@gmail.com>
2026-05-21 02:04:11 +0530

    fix : fix tenant base api

--- src/backend/ml_studio/organization/views.py
+++ src/backend/ml_studio/organization/views.py
@@ -5,9 +5,12 @@ from rest_framework import status
 from .selectors import Get_Orgs_For_User
 from .services import Create_Org
 from .serializers import Organization_List_Serializer , Organization_Create_Serializer
+from core.authentication import Firebase_Authentication_Middleware 
 # Create your views here.
 
+
 class Tenant_Base_API(APIView):
+    authentication_classes = [Firebase_Authentication_Middleware]
     def initial(self , request , *args , **kwargs):
         if request.user and request.user.is_authenticated:
             return super().initial(request,*args,**kwargs)
@@ -24,8 +27,8 @@ class Create_Org_API(Tenant_Base_API):
     def post(self , request):
         serializer = Organization_Create_Serializer(data = request.data)
         if serializer.is_valid():
-            organization = Create_Org(serializer.validated_data['name'] , user = request.user , type="team")
+            organization = Create_Org(name = serializer.validated_data['name'] , user = request.user , type="team")
             serializer = Organization_List_Serializer(organization) 
-            return Response({f"message : {serializer.data}" } , status = status.HTTP_200_OK)
+            return Response({f"message : organization created "} , status = status.HTTP_200_OK)
         else:
             return Response({f"error : {serializer.errors}" } , status = status.HTTP_400_BAD_REQUEST)
