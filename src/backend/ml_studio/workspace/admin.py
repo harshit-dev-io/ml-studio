5892de1b46d4e438a5164b8e89937bc3169a6ef1
Harshit <harshit392615@gmail.com>
2026-05-21 02:25:14 +0530

    feat : register workspace on admin dashboard

--- src/backend/ml_studio/workspace/admin.py
+++ src/backend/ml_studio/workspace/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Workspace
 # Register your models here.
+
+
+@admin.register(Workspace)
+class Workspace_admin(admin.ModelAdmin):
+    list_display = ["name" , "slug"]
