8a2899153de75760aa449e96947932a6e928fb88
Harshit <harshit392615@gmail.com>
2026-05-21 02:21:34 +0530

    feat : add membership check in tenant base api

--- src/backend/ml_studio/organization/views.py
+++ src/backend/ml_studio/organization/views.py
@@ -1,18 +1,31 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from organization.models import Membership
 from rest_framework import status 
 from .selectors import Get_Orgs_For_User
 from .services import Create_Org
 from .serializers import Organization_List_Serializer , Organization_Create_Serializer
 from core.authentication import Firebase_Authentication_Middleware 
+import logging
+from django.http import HttpResponseForbidden
+
 # Create your views here.
 
+logger = logging.getLogger(__name__)
 
 class Tenant_Base_API(APIView):
     authentication_classes = [Firebase_Authentication_Middleware]
     def initial(self , request , *args , **kwargs):
-        if request.user and request.user.is_authenticated:
+        if request.user and request.user.is_authenticated: 
+            try:
+                request.membership = Membership.objects.get(organization = request.organization , user = request.user)
+            except Membership.DoesNotExist :
+                return HttpResponseForbidden('No access to this organization')
+            except Exception as e :
+                logger.error(f"Error occurred while fetching organization {request.org_slug}: {e}")
+                request.membership = None
+                
             return super().initial(request,*args,**kwargs)
         else:
             return Response({"error": "Authentication required"}, status = status.HTTP_401_UNAUTHORIZED)
