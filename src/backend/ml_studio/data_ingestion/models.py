a8a5c9ee5154d7a01c1cb2ba26e95035d2b0b593
Harshit <harshit392615@gmail.com>
2026-06-02 23:12:52 +0530

    feat : add dataset model

--- src/backend/ml_studio/data_ingestion/models.py
+++ src/backend/ml_studio/data_ingestion/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from organization.models import Organization
 
 # Create your models here.
+class DataSet(models.Model):
+    organization_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    SOURCE_TYPE_CHOICES = (
+        ("kaggle" , "Kaggle"),
+        ("local" , "Local"),
+        # add more source types as needed
+    )
+
+    source_type = models.CharField(max_length=50 , choices=SOURCE_TYPE_CHOICES)
+    file_path = models.CharField(max_length=1024 , null = True , blank= True)  # path to the dataset file
+
+    STATUS_CHOICES = (
+        ("qurantine" , "Qurantine"),
+        ("infected" , "Infected"),
+        ("failed" , "Failed"),
+        ("processing" , "Processing"),
+        ("ready" , "Ready") 
+    )
+
+    status = models.CharField(max_length=256 , choices=STATUS_CHOICES)
+
+    column_schema = models.JSONField(default=dict) # (for ui purpose) initally a empty dict 
+    created_at = models.DateTimeField(auto_now_add==True)
